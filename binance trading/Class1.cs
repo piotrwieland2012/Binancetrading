b2ca3787c0c5df4058ea8b7939118793e0b9c304
Piotr W <piotrwieland2012@outlook.com>
2020-08-06 21:20:04 +0200

    Dodaj pliki projektów.

---
+++ binance trading/Class1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+public class Trading
+    userspace binance
+    BinanceClient.BinanceClient(string)
+    BinanceClient.BinanceClient(string, string, string)
+    BinanceClient.CreateDeleteRequest(string, System.Collections.Generic.Dictionary<string, object>)
+    BinanceClient.CreateGetRequest(string, System.Collections.Generic.Dictionary<string, object>)
+    BinanceClient.CreatePostRequest(string, System.Collections.Generic.Dictionary<string, object>)
+    BinanceClient.GetResponseMessage(RestSharp.IRestResponse)
+
+
+
+namespace binance_trading
+{
+    public class Class1
+    { public
+
+          [JsonProperty(DealerName);
+          [JsonProperty(Encryptor)];
+          [JsonProperty(ExchangeInfo];
+    }
+}
